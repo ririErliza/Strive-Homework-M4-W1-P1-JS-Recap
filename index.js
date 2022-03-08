5de0bb16e935d16012513afc70b9a402984b27f9
ririErliza <ryantiadam@yahoo.co.uk>
2022-03-08 11:53:02 +0100

    4-6

--- index.js
+++ index.js
@@ -43,28 +43,95 @@ console.log (removeACharacter ("Mouse",1));
 
 /* 4. Create a function to find and return the largest of three given integers.   */
 
+const whichLargest = function (r,s,t){
+    if (r>s && r>t){
+        return r;
+    } else if (s>r && s>t){
+        return s;
+    } else if (t>r && t>s){
+        return t;
+    }else{
+        return "not a number";
+    }
+}
+
+console.log(whichLargest(1,3,2));
+
+
+
+
+
 /*  5. Create a function to check if two numbers are in the range 40-60 or 70-100. 
 Return true if they do, return false if one (or both) don't.  */
 
+const areInRange = function (k,l){
+    if (k>40 && k<60 || l>40 && l<60 || k>70 && k<100 || l>70 && l<100){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(areInRange(5,1));
+
+
+
+
+
 /* 6. Create a function to create a new string composed of a specified number of copies of a given string. 
 Pass the string and the number of copies as parameters.   */
+
+
+const copyString = function (txt,int){
+    let newStr1 = txt.repeat(int);
+    return newStr1;
+}
+
+console.log(copyString("Why?",4));
+
+
+
 
 /* 7. Create a function to display the city name if the string begins with "Los" or "New". 
 Pass the city name as a parameter. 
 Return false if they start with a different string.   */
 
+
+
+
+
+
 /* 8. Create a function to calculate and return the sum of all elements from an array with 3 elements. 
 Pass the array as a parameter.   */
 
+
+
+
+
+
+
 /* 9. Create a function to test if an array of length 2 contains 1 OR 3. Return true is it does, false if it doesn't.   */
+
+
+
+
+
+
 
 
 /*  10. Create a function to test if an array of length 2 DOES NOT contain 1 or 3. 
 Return true if it doesn't, false if it does.  */
 
 
+
+
+
+
 /* 11. Create a function to find the longest string from a given array of strings. 
 Pass the array as parameter and return the longest string.   */
+
+
+
 
 
 /*  12. Create a function to find the types of a given angle:
@@ -73,19 +140,52 @@ Right angle ⇒ 90 degree. Return right
 Obtuse angle ⇒ between 90 and 180. Return obtuse
 Straight angle ⇒ 180 degrees. Return straight  */
 
+
+
+
+
+
+
 /* 13. Create a function to find and return the index of the greatest element of a given array of integers that you passed as a parameter  */
+
+
+
+
+
+
 
 /* 14. Create a function to find and return the largest even number from an array of integers that is passed a parameter.   */
 
+
+
+
+
+
+
 /*  15. Create a function to check from two given integers (passed as parameters) if one is positive and the other is negative. 
 Return true if that's the case, return false if it's not.  */
+
+
+
+
+
 
 /*  16. Create a function to create and return a new string where the first 3 characters are in lower case and the others are in upper case. 
 If the string's length is less than 3, convert the whole string into uppercase. 
 Pass the original string as a parameter. */
 
+
+
+
+
+
 /*  17. Create a function to calculate the sum of two integers (passed as parameters). 
 If the sum is in the 50-80 range, return 65, otherwise, return 80.  */
+
+
+
+
+
 
 /*  18. Create a function to convert a number (passed as a parameter) into a string, basing yourself on this example: 
 The number has 3 as a factor ⇒ return Diego 
@@ -95,5 +195,10 @@ If the number does not have 3,5, or 7, return the original number.
 ⚠️ The factor is an integer which evenly divides a number without leaving a remainder. 
 One number can have more than one factor, in that case you should return both names. 
 Ex: 15 has both 3 and 5 has factors: the function will return DiegoRiccardo  */
+
+
+
+
+
 
 /*  19. Create a function that that takes a phrase as a parameter and returns its acronym. Ex. British Broadcasting Corporation returns BBC  */
